61337cbe5062ec170c374038f641acf0322df913
Julien Veyssier <eneiluj@posteo.net>
2022-07-04 11:52:19 +0200

    bring back eslint and styleint on compilation
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
 	],
 	rules: {
 		'jsdoc/require-jsdoc': 'off',
-		'jsdoc/tag-lines': 'off'
+		'jsdoc/tag-lines': 'off',
+		'vue/first-attribute-linebreak': 'off',
+		'import/extensions': 'off'
 	}
 }
