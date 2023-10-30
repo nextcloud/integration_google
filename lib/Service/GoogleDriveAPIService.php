52f1707d1f89c54af8246ce508bd4f1cfa1899c8
Marcel Klehr <mklehr@gmx.net>
2023-10-30 16:22:37 +0100

    fix(ci/psalm)

--- lib/Service/GoogleDriveAPIService.php
+++ lib/Service/GoogleDriveAPIService.php
@@ -502,7 +502,11 @@ class GoogleDriveAPIService {
 		}
 
 		$stream = $logFile->fopen('a');
-		fwrite($stream, '1. Failed to download file: ' . $fileName . PHP_EOL);
+		if ($stream === false) {
+            $this->logger->error('Could not open log file');
+            return;
+        }
+        fwrite($stream, '1. Failed to download file: ' . $fileName . PHP_EOL);
 		fclose($stream);
 	}
 
