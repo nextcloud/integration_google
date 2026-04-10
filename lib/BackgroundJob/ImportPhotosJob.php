980221abf4ec90ffc0c7604ec5ca59add60004a7
Ahsan Ahmed <61637519+AhsanIsEpic@users.noreply.github.com>
2026-04-10 19:45:26 +0100

    fix(photos): address maintainer review comments
    - Update copyright headers in new PHP files to
      'Nextcloud GmbH and Nextcloud contributors 2025'
      with @author Ahsan Ahmed
    - Revert package-lock.json engine version bumps
      (these are auto-managed in separate PRs)
    - Refactor file download logic into GoogleAPIService::downloadAndSaveFile()
      so it can be reused by both GooglePhotosAPIService and GoogleDriveAPIService;
      remove the now-redundant private downloadAndSaveFile from GoogleDriveAPIService
    - Add ImportPhotosJob MissingOverrideAttribute to psalm-baseline.xml
    - Fix InvalidReturnType/InvalidReturnStatement in GooglePhotosAPIService
      via updated return-type docblocks and inline psalm-suppress annotations
    
    Signed-off-by: Ahsan Ahmed <61637519+AhsanIsEpic@users.noreply.github.com>

--- lib/BackgroundJob/ImportPhotosJob.php
+++ lib/BackgroundJob/ImportPhotosJob.php
@@ -6,8 +6,8 @@
  * This file is licensed under the Affero General Public License version 3 or
  * later. See the COPYING file.
  *
- * @author Julien Veyssier
- * @copyright Julien Veyssier 2020
+ * @author Ahsan Ahmed
+ * @copyright Nextcloud GmbH and Nextcloud contributors 2025
  */
 
 namespace OCA\Google\BackgroundJob;
